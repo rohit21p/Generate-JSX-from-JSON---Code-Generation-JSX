11b6e4e10e762919a02f8b57c506b6a8a2367568
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-09-26 20:54:55 +0530

    function returns string on the basis of obj given

--- src/index.js
+++ src/index.js
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 function generateCodeFromObject(obj){
     //return a code generated string
-    return `
-    <Clock style={{
-    "display": "flex",
-    flexDirection: "row",
-    justifyContent: "center"
-    }}>
-        <Hour />
-        <Minute style={{
-        color: "green",
-        fontSize: 30
-        }} />
-    </Clock>`
+    return `<${obj.name}${getStyle(obj.style)}${obj.children.length > 0 ? '>' : '/>'}${obj.children.length > 0 ? getChildrens(obj.children) : '' }${obj.children.length > 0 ? "</" + obj.name + ">" : ''}`;
    }
+
+function getStyle(obj) {
+    if (Object.keys(obj).length === 0) return '';
+    let ans = ' style={{';
+    Object.keys(obj).forEach(key => {
+        ans = ans + getCamelCase(key) + ":\"" + obj[key] + "\",";
+    });
+    ans = ans + '}}';
+    return ans;
+}
+
+function getCamelCase(str) {
+    return str.includes("-") ? (str.split("-")[0] + str.split("-")[1].charAt(1).toUpperCase() + str.split("-")[1].substring(2)) : str;
+}
+
+function getChildrens(arr) {
+    let ans = '';
+    arr.forEach(el => {
+        ans += generateCodeFromObject(el)
+    })
+    return ans;
+}
    
 module.exports=generateCodeFromObject;
 
