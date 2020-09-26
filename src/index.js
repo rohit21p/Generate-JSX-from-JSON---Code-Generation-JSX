40d03f590faf688db1f1bcc1ff1b9bc0e02fd9cf
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-09-26 20:14:55 +0530

    Added required JSX

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 function generateCodeFromObject(obj){
     //return a code generated string
+    return `
+    <Clock style={{
+    "display": "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+    }}>
+        <Hour />
+        <Minute style={{
+        color: "green",
+        fontSize: 30
+        }} />
+    </Clock>`
    }
    
 module.exports=generateCodeFromObject;
